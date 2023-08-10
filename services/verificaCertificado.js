90f5debaa89b9664dc83ceab4ab3798dd2c0d26d
offhawk <notorioushawk123@gmail.com>
2023-08-10 14:05:20 -0300

    Verificar certificado

---
+++ services/verificaCertificado.js
@@ -0,0 +1,69 @@
+import { db } from "../pages/login/assets/js/firebase.js";
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+let btnVer = document.querySelector('#btn-verificar');
+btnVer.addEventListener('click', verificaCert);
+
+
+async function verificaCert() {
+
+    let inputCertEl = document.querySelector('#input-cert').value;
+    let spanError = document.querySelector('#span-error')
+    let spanNome = document.querySelector('#user-cert')
+    let spanTurma = document.querySelector('#turma-cert')
+
+    if(inputCertEl != ""){
+        let certQuery = query(
+            collection(db, "certificado"),
+            where("id", "==", inputCertEl)
+          );
+          const querySnapshot = await getDocs(certQuery);
+      
+          let certDoc;
+      
+          querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            certDoc = doc.data();
+        });
+    
+        console.log(certDoc)
+    
+        if(certDoc){
+
+            let userQuery = query(
+                collection(db, "aluno"),
+                where("uid", "==", certDoc.alunoId)
+              );
+              const querySnapshotUser = await getDocs(userQuery);
+          
+              let userDoc;
+          
+              querySnapshotUser.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                userDoc = doc.data();
+            });
+
+            spanNome.textContent = userDoc.nome;
+            spanTurma.textContent = userDoc.curso;
+
+            $('#modalSuccess').modal('show')
+
+        } else {
+            $('#modalError').modal('show')
+        }
+    } else {
+        spanError.textContent = "Preencha o Campo."
+    }
+
+    
+
+}
+
